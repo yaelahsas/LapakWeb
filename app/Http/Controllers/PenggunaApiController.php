19f96351dca9f76eebebcd381b422306896584e5
ninik1901 <ninikwulandari232@gmail.com>
2021-08-19 19:35:33 +0700

    menambahkan api kategori ebook dan ebook terbaru

--- app/Http/Controllers/PenggunaApiController.php
+++ app/Http/Controllers/PenggunaApiController.php
@@ -8,14 +8,14 @@ use App\User;
 class PenggunaApiController extends Controller
 {
         public function tampilUser(Request $request){
-            $id=$request->id;
+            $id=$request->pengguna_id;
 
             $pengguna = User::where('id',$id)->get();
             return response()->json($pengguna, 200);
     }
 
     public function ubahProfilUser(Request $request){
-        $id=$request->id;
+        $id=$request->pengguna_id;
 
         $profil= User::find($id);
         $profil->update([
